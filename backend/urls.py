d7cf9c6a8d2ce4ec008861e064749a4af47f2b7f
Bharat Kalluri <bharatkalluri@protonmail.com>
2024-08-23 20:54:54 +0530

    added crud on subscriptions

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,42 @@
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    # signup
     path("signup", views.SignUpView.as_view(), name="signup"),
+    # subscriptions
+    path(
+        "subscriptions/list",
+        login_required(views.SubscriptionListView.as_view()),
+        name="subscription_list",
+    ),
+    path(
+        "subscriptions/shared",
+        login_required(views.SharedSubscriptionListView.as_view()),
+        name="subscription_shared",
+    ),
+    path(
+        "subscriptions/discover",
+        login_required(views.DiscoverSubscriptionListView.as_view()),
+        name="subscription_discover",
+    ),
+    path(
+        "subscriptions/add",
+        login_required(views.SubscriptionAddView.as_view()),
+        name="subscription_add",
+    ),
+    path(
+        "subscriptions/update/<int:pk>",
+        login_required(views.SubscriptionUpdateView.as_view()),
+        name="subscription_update",
+    ),
+    path(
+        "subscriptions/delete/<int:pk>",
+        login_required(views.SubscriptionDeleteView.as_view()),
+        name="subscription_delete",
+    ),
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.views import generic
 from django.views.generic import CreateView
 
-from backend.models import Subscription
+from backend.models import Subscription, Community
 
 
 class RegistrationForm(UserCreationForm):
@@ -16,9 +17,84 @@ class SignUpView(CreateView):
     template_name = "registration/signup.html"
 
 
-class IndexView(generic.ListView):
+class IndexView(generic.TemplateView):
     template_name = "backend/index.html"
+
+
+class SubscriptionListView(generic.ListView):
+    template_name = "backend/subscription/list.html"
     context_object_name = "subscriptions"
 
     def get_queryset(self):
-        return Subscription.objects.all()
+        return Subscription.objects.filter(owner=self.request.user)
+
+
+class SharedSubscriptionListView(generic.ListView):
+    template_name = "backend/subscription/shared.html"
+    context_object_name = "subscriptions"
+
+    def get_queryset(self):
+        return Subscription.objects.filter(shared_to=self.request.user)
+
+
+class DiscoverSubscriptionListView(generic.ListView):
+    template_name = "backend/subscription/discover.html"
+    context_object_name = "subscriptions"
+
+    def get_queryset(self):
+        # TODO: horrible query, but it works. fix it later
+        communities_the_user_belongs_to = Community.objects.filter(
+            members=self.request.user
+        )
+        all_users_of_communities_the_user_belongs_to = [
+            user
+            for community in communities_the_user_belongs_to
+            for user in community.members.all()
+        ]
+        all_subscriptions_of_users_in_communities_the_user_belongs_to = (
+            Subscription.objects.filter(
+                owner__in=all_users_of_communities_the_user_belongs_to
+            )
+        )
+        return all_subscriptions_of_users_in_communities_the_user_belongs_to.exclude(
+            owner=self.request.user
+        ).exclude(shared_to=self.request.user)
+
+
+class SubscriptionAddView(generic.CreateView):
+    template_name = "backend/subscription/add.html"
+    model = Subscription
+    fields = ["name", "is_active", "shared_to"]
+    success_url = reverse_lazy("subscription_list")
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class SubscriptionUpdateView(generic.UpdateView):
+    template_name = "backend/subscription/update.html"
+    model = Subscription
+    fields = ["name", "is_active", "shared_to"]
+    success_url = reverse_lazy("subscription_list")
+
+    def get_queryset(self):
+        return Subscription.objects.filter(owner=self.request.user)
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+
+        form.fields["shared_to"].queryset = form.fields["shared_to"].queryset.exclude(
+            id=self.request.user.id
+        )
+
+        return form
+
+
+class SubscriptionDeleteView(generic.DeleteView):
+    template_name = "backend/subscription/delete.html"
+    model = Subscription
+    success_url = reverse_lazy("subscription_list")
+
+    def get_queryset(self):
+        return Subscription.objects.filter(owner=self.request.user)
