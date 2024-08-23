8afc35855b7e319f0bcdcb93cf8b76ba816519d0
Bharat Kalluri <bharatkalluri@protonmail.com>
2024-08-24 00:25:10 +0530

    move to cud templates & add functionality to add & remove members

--- backend/urls.py
+++ backend/urls.py
@@ -6,8 +6,44 @@ from . import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    # signup
+    # auth
     path("signup", views.SignUpView.as_view(), name="signup"),
+    path("logout", views.LogoutView.as_view(), name="custom_logout"),
+    # communities
+    path(
+        "communities/list",
+        login_required(views.CommunityListView.as_view()),
+        name="community_list",
+    ),
+    path(
+        "communities/add",
+        login_required(views.CommunityAddView.as_view(extra_context={"view": "add"})),
+        name="community_add",
+    ),
+    path(
+        "communities/update/<int:pk>",
+        login_required(
+            views.CommunityUpdateView.as_view(extra_context={"view": "update"})
+        ),
+        name="community_update",
+    ),
+    path(
+        "communities/join/<int:pk>",
+        login_required(views.CommunityAddMemberView.as_view()),
+        name="community_add_member",
+    ),
+    path(
+        "communities/leave/<int:pk>",
+        login_required(views.CommunityRemoveMemberView.as_view()),
+        name="community_remove_member",
+    ),
+    path(
+        "communities/delete/<int:pk>",
+        login_required(
+            views.CommunityDeleteView.as_view(extra_context={"view": "delete"})
+        ),
+        name="community_delete",
+    ),
     # subscriptions
     path(
         "subscriptions/list",
@@ -26,17 +62,23 @@ urlpatterns = [
     ),
     path(
         "subscriptions/add",
-        login_required(views.SubscriptionAddView.as_view()),
+        login_required(
+            views.SubscriptionAddView.as_view(extra_context={"view": "add"})
+        ),
         name="subscription_add",
     ),
     path(
         "subscriptions/update/<int:pk>",
-        login_required(views.SubscriptionUpdateView.as_view()),
+        login_required(
+            views.SubscriptionUpdateView.as_view(extra_context={"view": "update"})
+        ),
         name="subscription_update",
     ),
     path(
         "subscriptions/delete/<int:pk>",
-        login_required(views.SubscriptionDeleteView.as_view()),
+        login_required(
+            views.SubscriptionDeleteView.as_view(extra_context={"view": "delete"})
+        ),
         name="subscription_delete",
     ),
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.views import generic
@@ -15,6 +16,10 @@ class SignUpView(CreateView):
     form_class = RegistrationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+class LogoutView(generic.TemplateView):
+    template_name = "registration/logout.html"
 
 
 class IndexView(generic.TemplateView):
@@ -62,7 +67,7 @@ class DiscoverSubscriptionListView(generic.ListView):
 
 
 class SubscriptionAddView(generic.CreateView):
-    template_name = "backend/subscription/add.html"
+    template_name = "backend/subscription/cud.html"
     model = Subscription
     fields = ["name", "is_active", "shared_to"]
     success_url = reverse_lazy("subscription_list")
@@ -73,7 +78,7 @@ class SubscriptionAddView(generic.CreateView):
 
 
 class SubscriptionUpdateView(generic.UpdateView):
-    template_name = "backend/subscription/update.html"
+    template_name = "backend/subscription/cud.html"
     model = Subscription
     fields = ["name", "is_active", "shared_to"]
     success_url = reverse_lazy("subscription_list")
@@ -92,9 +97,114 @@ class SubscriptionUpdateView(generic.UpdateView):
 
 
 class SubscriptionDeleteView(generic.DeleteView):
-    template_name = "backend/subscription/delete.html"
+    template_name = "backend/subscription/cud.html"
     model = Subscription
     success_url = reverse_lazy("subscription_list")
 
     def get_queryset(self):
         return Subscription.objects.filter(owner=self.request.user)
+
+
+class CommunityListView(generic.ListView):
+    template_name = "backend/community/list.html"
+    context_object_name = "communities"
+
+    def get_queryset(self):
+        return Community.objects.filter(owner=self.request.user)
+
+
+class CommunityUpdateView(generic.UpdateView):
+    template_name = "backend/community/cud.html"
+    model = Community
+    fields = ["name", "is_active"]
+    success_url = reverse_lazy("community_list")
+
+    def get_queryset(self):
+        return Community.objects.filter(owner=self.request.user)
+
+
+class UpdateCommunityMembersForm(forms.Form):
+    user_name = forms.CharField(label="username", max_length=100)
+
+
+class CommunityAddMemberView(generic.FormView):
+    template_name = "backend/community/add_member.html"
+    form_class = UpdateCommunityMembersForm
+    success_url = reverse_lazy("community_list")
+
+    def get_user(self, user_name):
+        try:
+            return User.objects.get(username=user_name)
+        except User.DoesNotExist:
+            return None
+
+    def form_valid(self, form):
+        user_name = form.cleaned_data["user_name"]
+        community_id = self.kwargs["pk"]
+        user_with_mobile_number: User | None = self.get_user(user_name=user_name)
+        if user_with_mobile_number is None:
+            form.add_error("user_name", "User not found")
+            return super().form_invalid(form)
+
+        community = Community.objects.get(pk=community_id)
+        if not community.members.filter(
+            username=user_with_mobile_number.username
+        ).exists():
+            community.members.add(user_with_mobile_number)
+            community.save()
+        return super().form_valid(form)
+
+
+class RemoveCommunityMembersForm(forms.Form):
+    user_name = forms.CharField(label="username", max_length=100)
+
+
+class CommunityRemoveMemberView(generic.FormView):
+    template_name = "backend/community/remove_member.html"
+    form_class = RemoveCommunityMembersForm
+    success_url = reverse_lazy("community_list")
+
+    def get_user(self, user_name):
+        try:
+            return User.objects.get(username=user_name)
+        except User.DoesNotExist:
+            return None
+
+    def form_valid(self, form):
+        user_name = form.cleaned_data["user_name"]
+        community_id = self.kwargs["pk"]
+        user_with_user_name: User | None = self.get_user(user_name=user_name)
+        if user_with_user_name.username == self.request.user.username:
+            form.add_error(
+                "user_name", "you cannot remove yourself from your community my friend"
+            )
+            return super().form_invalid(form)
+        if user_with_user_name is None:
+            form.add_error("user_name", "User not found")
+            return super().form_invalid(form)
+
+        community = Community.objects.get(pk=community_id)
+        if community.members.filter(username=user_with_user_name.username).exists():
+            community.members.remove(user_with_user_name)
+            community.save()
+        return super().form_valid(form)
+
+
+class CommunityAddView(generic.CreateView):
+    template_name = "backend/community/cud.html"
+    model = Community
+    fields = ["name"]
+    success_url = reverse_lazy("community_list")
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class CommunityDeleteView(generic.DeleteView):
+    template_name = "backend/community/cud.html"
+    model = Community
+    success_url = reverse_lazy("community_list")
+
+    def get_queryset(self):
+        return Community.objects.filter(owner=self.request.user)
