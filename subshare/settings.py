908401d4249678040494c3c5c6b15c443d414176
Bharat Kalluri <bharatkalluri@protonmail.com>
2024-08-24 02:15:03 +0530

    update allowed hosts & csrf origins

--- subshare/settings.py
+++ subshare/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["subshare.fly.dev"]
+CSRF_TRUSTED_ORIGINS = ["https://subshare.fly.dev"]
 
 # Application definition
 
