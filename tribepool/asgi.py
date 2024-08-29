bf514b025509afcd4b481b22b7c3d7c9a15ff9a2
Bharat Kalluri <bharatkalluri@protonmail.com>
2024-08-29 23:50:56 +0530

    renamed the product to tribepool. updated the landing page

--- tribepool/asgi.py
+++ tribepool/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for subshare project.
+ASGI config for tribepool project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "subshare.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tribepool.settings")
 
 application = get_asgi_application()
--- tribepool/urls.py
+++ tribepool/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for subshare project.
+URL configuration for tribepool project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
