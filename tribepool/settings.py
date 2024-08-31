f5010345c06f871c91f2009b2d6efd51a83beedd
Bharat Kalluri <bharatkalluri@protonmail.com>
2024-08-31 16:12:21 +0530

    add back https settings

--- tribepool/settings.py
+++ tribepool/settings.py
@@ -18,6 +18,15 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Ensure Django uses HTTPS
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# django-allauth specific settings
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = "https"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
